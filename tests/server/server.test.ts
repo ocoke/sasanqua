df078e67fe11034391ce3b1b1b6232ec81e54751
Jun Y. <awa@outlook.ie>
2024-01-10 06:06:46 +0000

    test: combine test files

--- tests/server/server.test.ts
+++ tests/server/server.test.ts
@@ -1,3 +1,5 @@
+// get server api from environment
+
 const basePort = process.env.SASANQUA_TEST_PORT || 3000
 
 import { describe, expect, test } from 'vitest'
@@ -18,6 +20,72 @@ const testSiteData = {
         "speedInsights": true,
     }
 }
+
+
+describe('Sasanqua: User', () => {
+    test('Sign up', async () => {
+        const response = await fetch(`${server}/user/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: 'test',
+                password: 'test'
+            })
+        })
+        const data = await response.json()
+        expect(data.code).toEqual(200)
+        expect(data.data).toBeTypeOf('string')
+    })
+
+    test('Sign in with correct data', async () => {
+        const response = await fetch(`${server}/user/signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: 'test',
+                password: 'test'
+            })
+        })
+        const data = await response.json()
+        expect(data.code).toEqual(200)
+        expect(data.data).toBeTypeOf('string')
+        token = data.data
+        
+    })
+
+    test('Sign in with token', async () => {
+        console.log(token)
+        const response = await fetch(`${server}/user/token`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        })
+        const data = await response.json()
+        expect(data.code).toEqual(200)
+    })
+
+    test('Sign in with incorrect data', async () => {
+        const response = await fetch(`${server}/user/signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: 'test',
+                password: 'test2'
+            })
+        })
+        const data = await response.json()
+        expect(data.code).toEqual(400)
+    })
+
+})
 
 describe('Sasanqua: Site', () => {
     test('Sign in', async () => {
