8a8e6b6e7aad03f289cd01973e99bfecc08befaa
Jun Y <awa@outlook.ie>
2024-03-17 18:48:41 -0700

    New translations en-us.ts (Chinese Simplified)

--- locales/zh-CN.ts
+++ locales/zh-CN.ts
@@ -1,66 +1,66 @@
 export default {
   home: {
-    gotodashboard: 'Go to Dashboard'
+    gotodashboard: "转到仪表板"
   },
   signin: {
-    signinto: 'Sign in to',
-    username: 'Username',
-    password: 'Password',
-    noaccount: 'Didn\'t have an account? Sign up.',
-    signin: 'Sign in'
+    signinto: "登录",
+    username: "用户名",
+    password: "密码",
+    noaccount: "没有帐户？注册。",
+    signin: "登录"
   },
   dashboard: {
-    dashboard: 'Dashboard',
-    viewall: 'View All',
-    viewdetails: 'View Details',
-    about: 'About'
+    dashboard: "仪表板",
+    viewall: "查看全部",
+    viewdetails: "查看详情",
+    about: "关于"
   },
   settings: {
-    settings: 'Settings'
+    settings: "设置"
   },
   websites: {
-    websites: 'Websites',
-    add: 'Add Website'
+    websites: "网站",
+    add: "添加网站"
   },
   website: {
-    views: 'Views',
-    visitors: 'Visitors',
-    avgtime: 'Avg Visit Time',
-    today: 'Today',
-    thisweek: 'This Week',
-    data: 'Data',
-    charts: 'Charts',
-    pages: 'Pages',
-    titles: 'Titles',
-    queries: 'Queries',
-    referrers: 'Referrers',
-    countries: 'Countries',
-    languages: 'Languages',
-    browsers: 'Browsers',
-    os: 'OS',
-    devices: 'Devices',
-    edit: 'Edit',
-    currentVisitors: 'current visitors',
-    range: 'Range',
+    views: "访问",
+    visitors: "访客",
+    avgtime: "平均访问时间",
+    today: "今天",
+    thisweek: "本周",
+    data: "数据",
+    charts: "图表",
+    pages: "页面",
+    titles: "标题",
+    queries: "查询",
+    referrers: "引用",
+    countries: "国家",
+    languages: "语言",
+    browsers: "浏览器",
+    os: "操作系统",
+    devices: "设备",
+    edit: "编辑",
+    currentVisitors: "当前访客",
+    range: "范围",
     timeRange: {
-      last24h: 'Last 24 Hours',
-      last7: 'Last 7 Days',
-      last30: 'Last 30 Days',
-      last90: 'Last 90 Days',
-      lasty: 'Last Year'
+      last24h: "过去 24 小时",
+      last7: "过去 7 天",
+      last30: "过去 30 天",
+      last90: "过去 90 天",
+      lasty: "去年"
     }
   },
   data: {
-    addFilter: 'Add Filter',
-    speedInsights: 'Speed Insights',
-    is: 'is',
-    isNot: 'is not',
-    delete: 'Delete',
-    and: 'And',
-    or: 'Or',
-    save: 'Save'
+    addFilter: "添加过滤器",
+    speedInsights: "速度洞察",
+    is: "是",
+    isNot: "不是",
+    delete: "删除",
+    and: "和",
+    or: "或",
+    save: "保存"
   },
   error: {
-    error: 'Error'
+    error: "错误"
   }
 };
